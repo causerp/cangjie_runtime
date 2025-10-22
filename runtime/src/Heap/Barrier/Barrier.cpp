5a7b6f7c470f5587557d8aed7a95526927d1f2d9
x00619772 <xiaohan24@huawei.com>
2025-10-22 15:02:51 +0800

    feat: support gc for arm32
    Signed-off-by: xiaohan <xiaohan24@huawei.com>

--- runtime/src/Heap/Barrier/Barrier.cpp
+++ runtime/src/Heap/Barrier/Barrier.cpp
@@ -177,7 +177,6 @@ void Barrier::ReadStruct(MAddress dst, BaseObject* obj, MAddress src, size_t siz
 {
     size_t dstSize = size;
     size_t srcSize = size;
-#ifndef __arm__
     if (obj != nullptr) {
         obj->ForEachRefInStruct(
             [this, obj](RefField<false>& field) {
@@ -190,7 +189,6 @@ void Barrier::ReadStruct(MAddress dst, BaseObject* obj, MAddress src, size_t siz
             },
             src, src + size);
     }
-#endif
 
     CHECK_DETAIL(memcpy_s(reinterpret_cast<void*>(dst), dstSize, reinterpret_cast<void*>(src), srcSize) == EOK,
                  "read struct memcpy_s failed");
