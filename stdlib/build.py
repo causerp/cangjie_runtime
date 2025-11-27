2799622543ee1b4d0c93b3f8ceebe25048362964
jiyuan_99 <jiyuan_v@163.com>
2025-11-27 09:44:06 +0000

    feat: std ios-simulator-x86_64 build
    Signed-off-by: jiyuan_99 <jiyuan_v@163.com>

--- stdlib/build.py
+++ stdlib/build.py
@@ -77,6 +77,8 @@ def generate_cmake_defs(args):
             toolchain_file = "mingw_x86_64_toolchain.cmake"
         elif args.target == "arm64-apple-ios11-simulator":
             toolchain_file = "ios_simulator_arm64_toolchain.cmake"
+        elif args.target == "x86_64-apple-ios11-simulator":
+            toolchain_file = "ios_simulator_x86_64_toolchain.cmake"
         elif args.target == "arm64-apple-ios11":
             toolchain_file = "ios_arm64_toolchain.cmake"
         elif "aarch64-linux-android" in args.target:
