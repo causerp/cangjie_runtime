98ce81c80babe3dcd4642887c092dcdc73135515
lrb <liruibo6@h-partners.com>
2025-12-03 09:56:16 +0800

    fix: bugfix generic
    1. bugfix CheckMethodActualArgs
       Add validation to determine if generic type parameters
       originate from the function's containing class
    2. bugfix struct init with Generic
       The initialization functions for generic and non-generic structs
       have different input parameters, adapt to generic scenarios.
    
    Signed-off-by: lrb <liruibo6@h-partners.com>

--- runtime/src/ObjectModel/MethodInfo.cpp
+++ runtime/src/ObjectModel/MethodInfo.cpp
@@ -226,6 +226,9 @@ TypeInfo* MethodInfo::GetActualTypeFromGenericTypeImpl(GenericTypeInfo* genericT
         TypeInfo* ti = reinterpret_cast<TypeInfo*>(genericTi->GetGenericArg(idx));
         if (ti->IsGeneric()) {
             TypeInfo* argTi = GetActualTypeFromGenericType(reinterpret_cast<GenericTypeInfo*>(ti), genericArgs);
+            if (argTi == nullptr) {
+                argTi = GetActualTypeFromGenericType(reinterpret_cast<GenericTypeInfo*>(ti), nullptr);
+            }
             if (argTi == nullptr || argTi->IsGeneric()) {
                 free(tmp);
                 return nullptr;
@@ -517,7 +520,7 @@ void* MethodInfo::ApplyCJMethod(ObjRef instanceObj, void* genericArgs, void* act
         argValues.AddReference(instanceObj);
     } else {
         TypeInfo* ti = declaringTi;
-        if (IsInitializer() && ti->IsClass()) {
+        if (IsInitializer() && (ti->IsClass() || (ti->IsStruct() && ti->IsGenericTypeInfo()))) {
             U32 size = ti->GetInstanceSize();
             instanceObj = ObjectManager::NewObject(declaringTi,
                 MRT_ALIGN(size + sizeof(TypeInfo*), sizeof(TypeInfo*)), AllocType::RAW_POINTER_OBJECT);
@@ -555,7 +558,7 @@ void* MethodInfo::ApplyCJMethod(ObjRef instanceObj, void* genericArgs, void* act
     if (HasSRetWithUnknowGenericStruct()) {
         return ret.ref;
     }
-    if (IsInitializer() && declaringTi->IsClass()) {
+    if (IsInitializer() && (declaringTi->IsClass() || (declaringTi->IsStruct() && declaringTi->IsGenericTypeInfo()))) {
         return instanceObj;
     } else if (IsInitializer() && declaringTi->IsStruct()) {
         ret.ref = instanceObj;
