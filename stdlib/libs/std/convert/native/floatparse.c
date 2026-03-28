2b57a5408d84509b536d49cfa943c8c83af197b5
miushuxin <yaoji2@h-partners.com>
2026-03-28 09:50:13 +0800

    fix: use strtod_l with C locale for locale-independent float parsing
    Signed-off-by: miushuxin <yaoji2@h-partners.com>

--- stdlib/libs/std/convert/native/floatparse.c
+++ stdlib/libs/std/convert/native/floatparse.c
@@ -6,9 +6,20 @@
  * See https://cangjie-lang.cn/pages/LICENSE for license information.
  */
 
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <locale.h>
+
+#if defined(__APPLE__)
+#include <xlocale.h>
+#define HAS_STRTOD_L 1
+#elif defined(__linux__)
+#define HAS_STRTOD_L 1
+#else
+#define HAS_STRTOD_L 0
+#endif
 
 extern double* CJ_STRTOD(char* str)
 {
@@ -17,7 +28,22 @@ extern double* CJ_STRTOD(char* str)
     }
     char* p = NULL;
     errno = 0;
-    double value = strtod(str, &p);
+    double value;
+
+#if HAS_STRTOD_L
+    static locale_t c_locale = NULL;
+    if (c_locale == NULL) {
+        c_locale = newlocale(LC_NUMERIC_MASK, "C", (locale_t)0);
+    }
+    if (c_locale != NULL) {
+        value = strtod_l(str, &p, c_locale);
+    } else {
+        value = strtod(str, &p);
+    }
+#else
+    value = strtod(str, &p);
+#endif
+
     if (p == NULL || strlen(p) > 0 || errno == ERANGE) {
         return NULL;
     }
